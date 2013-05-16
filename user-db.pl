692e6dfb7743c3ebae82de5f54bd383e733d359b
Roger Sen <roger.sen@gmail.com>
2013-05-16 12:06:46 +0200

    Split dames and added user-db.pl

---
+++ user-db.pl
@@ -0,0 +1,3 @@
+% user( player, email, password )
+
+user( jldq, 'roger.sen@gmail.com', 'pepito12').
--- world-db.pl
+++ world-db.pl
@@ -1,56 +1,102 @@
 % player( name, sibling_rank, rank, orphan-p, funds, allowance, father_title, title, soclvl).
-player(jldq, 'Jean Luc d\'Quintillon', first, nobleman, no, 500, 100, marquis, marquis, 8).
-player(mdo, 'Marc d\'Orc', first, nobleman, yes, 500, 100, duke, duke, 9).
+player(jldq, 'Jean Luc d''Quintillon', first, nobleman, no, 500, 100, marquis, marquis, 8).
+player(mdo, 'Marc d''Orc', first, nobleman, yes, 500, 100, duke, duke, 9).
 
 % club(abbreviation, name, min_soc_lvl, dues, status_pts_month, house_limit, gambling_divisor).
 
-club(eo, 'L\'Epée D\'Or', 12, 30, 8, 9999, 500).
+club(eo, 'L''Epée D''Or', 12, 30, 8, 9999, 500).
 club(ch, 'Les Chasseurs', 9, 20, 6, 200, 300).
 club(gm, 'Le Garde Montante', 7, 20, 5, 250, 300).
 club(tb, 'Les Tuiles Bleues', 7, 15, 4, 150, 200).
-club(ca, 'Le Crapaud et l\'Apricot', 5, 10, 3, 100, 150).
+club(ca, 'Le Crapaud et l''Apricot', 5, 10, 3, 100, 150).
 club(pr, 'Phillippe Le Rouge', 3, 5, 2, 50, 100).
 
 
 % dame(num, name, soc_lvl, [attributes], lover, husband, num_kids).
 
-dame( 1, 'Christine Daé', 12, [beautiful, rich], jlh, none, 0).
-dame( 2, 'Charlotte Mousse', 22, [rich, married], none, oda, 3).
-dame( 3, 'Silvie Cristelle', 4, [beautiful], none, none, 0).
-dame( 4, 'Marie Bonbon', 5, [beautiful], none, none, 0).
-dame( 5, 'Ivonne Marceau', 3, [rich], none, none, 0).
-dame( 6, 'Mussette d\'Envion', 9, [rich, married], none, none, 1).
-dame( 7, 'Josephine Bidette', 5, [], none, none, 0).
-dame( 8, 'Victorie Labelle', 14, [], none, none, 0).
-dame( 9, 'Magdalene du Croissant', 20, [beautiful, rich], none, none, 0).
-dame( 10, 'Constance Lacroix', 20, [rich, married], none, jlm, 0).
-dame( 11, 'Giovanna Pascalli', 7, [], none, none, 0).
-dame( 12, 'Turandot de Lagarde', 12, [beautiful], none, none, 0).
-dame( 13, 'Claudette Gorloise', 8, [beautiful], none, none, 0).
-dame( 14, 'Juliette DeClaude', 5, [], none, none, 0).
-dame( 15, 'Francine Davelet', 4, [rich], none, none, 0).
-dame( 16, 'Georgette d\'Avignon', 15, [rich], gdf, none, 0).
-dame( 17, 'Charlotte Pézet', 7, [beautiful, rich], cbc, none, 0).
-dame( 18, 'Cosette Lamoire', 16, [influential], none, none, 0).
-dame( 19, 'Louise Codolon', 15, [beautiful], none, none, 1).
-dame( 20, 'Evelyne Garabedien', 16, [beautiful, rich], jpr, none, 0).
-dame( 21, 'Marie-Claire Gassol', 7, [], pch, none, 0).
-dame( 22, 'Claire Lagaine', 11, [beautiful], none, none, 0).
-dame( 23, 'Justine de Sade', 15, [beautiful], none, none, 0).
-dame( 24, 'Michelle Gillette', 8, [], none, none, 0).
-dame( 25, 'Isabelle Girard', 11, [], none, none, 0).
-dame( 26, 'Marianne Bientôt', 6, [rich], none, none, 0).
-dame( 27, 'Lili Montparnasse', 11, [beautiful, married], none, ldp, 1).
-dame( 28, 'Natasha Kinski', 13, [], none, none, 0).
-dame( 29, 'Melissa Gilbert', 7, [beautiful], none, none, 0).
-dame( 30, 'Ute Hoffmann', 13, [], none, none, 0).
-dame( 31, 'Isabel d\'Artois', 7, [], none, none, 0).
-dame( 32, 'Eleonor d\'Yberville', 18, [beautiful, rich], jlb, none, 0).
+dame( cd, 'Christine Daé', 12).
+dame( cm, 'Charlotte Mousse', 22).
+dame( sc, 'Silvie Cristelle', 4).
+dame( mb, 'Marie Bonbon', 5).
+dame( im, 'Ivonne Marceau', 3).
+dame( mde, 'Mussette d''Envion', 9).
+dame( jb, 'Josephine Bidette', 5).
+dame( vl, 'Victorie Labelle', 14).
+dame( mdc, 'Magdalene du Croissant', 20).
+dame( cl, 'Constance Lacroix', 20.
+dame( gp, 'Giovanna Pascalli', 7).
+dame( tdl, 'Turandot de Lagarde', 12).
+dame( cg, 'Claudette Gorloise', 8).
+dame( jdc, 'Juliette DeClaude', 5).
+dame( fdv, 'Francine Davelet', 4).
+dame( gdv, 'Georgette d''Avignon', 15).
+dame( cp, 'Charlotte Pézet', 7).
+dame( cl, 'Cosette Lamoire', 16).
+dame( lc, 'Louise Codolon', 15).
+dame( eg, 'Evelyne Garabedien', 16).
+dame( mcg, 'Marie-Claire Gassol', 7).
+dame( cl, 'Claire Lagaine', 11).
+dame( jds, 'Justine de Sade', 15).
+dame( mg, 'Michelle Gillette', 8).
+dame( ig, 'Isabelle Girard', 11).
+dame( mb, 'Marianne Bientôt', 6).
+dame( lm, 'Lili Montparnasse', 11).
+dame( nk, 'Natasha Kinski', 13).
+dame( mg, 'Melissa Gilbert', 7).
+dame( uh, 'Ute Hoffmann', 13).
+dame( ida, 'Isabel d''Artois', 7).
+dame( edy, 'Eleonor d''Yberville', 18).
 
-married_not_player( Dame_id, Name ) :-
-	dame(Dame_id, Name, _, Attributes, _, Husband, _),
-	member(married, Attributes),
-	Husband == none.
+beautiful(cd).
+rich(cd).
+rich(cm).
+rich(im).
+rich(mde).
+rich(mdc).
+rich(cl).
+rich( fdv ).
+rich( gdv ).
+rich( cp ).
+rich( eg ).
+rich( mb ).
+rich( edy ).
+beautiful(tdl).
+beautiful(cg).
+beautiful(cp).
+beautiful(lc).
+beautiful(eg).
+beautiful(cl).
+beautiful(jds).
+beautiful(lm).
+beautiful(mg).
+beautiful(edy).
+beutiful(mb).
+beutiful(mdc).
+influential(cl).
+
+
+
+
+lover( jlh, cd ).
+lover( gdf, gdv ).
+lover( cbc, cp ).
+lover( jpr, eg ).
+lover( pch, mcg ).
+lover( jlb, edy ).
+married( oda, cm ).
+married( jlm, cl ).
+married( ldp, lm ).
+kids( none, mde, 1 ).
+kids( none, lc, 1 ).
+kids( ldp, lm, 1).
+
+is_married( X ) :- is_married(X, _).
+is_married( X, Y ) :- married(X, Y); married(Y, X).
+is_lover( X ) :- is_lover(X, _).
+is_lover( X, Y ) :- lover(X, Y); lover(Y, X).
+has_kids( X, Num ) :- kids( X, _, Num); kids(_, X, Num).
+
+
 
 % regiment(abbreviation, name, arm, col, tcol, [majs], [capts], [ncos], [soldiers])
 
